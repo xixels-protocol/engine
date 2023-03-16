f6392d015f36bee136a37a15c1ce3e9a62cf2f33
ryan <ryan.playeralpha@gmail.com>
2023-03-16 13:05:21 +0800

    Update Sphere.cpp

--- core/math/Sphere.cpp
+++ core/math/Sphere.cpp
@@ -4,6 +4,10 @@ using namespace Xixels;
 
 _bool Sphere::Intersect( const Plane& p, const Vector3& dir, _float& dist ) const
 {
-	// TODO.
-	return _false;
+	dist = 0.0f;
+
+	if ( Math::Abs( p.Distance( c ) ) <= Math::Abs( r ) )
+		return _true;
+
+	return Ray( c, dir ).Intersect( Plane( p.a, p.b, p.c, p.d - Math::Abs( r ) ), dist );
 }
