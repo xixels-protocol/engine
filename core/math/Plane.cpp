f154b755f5adc379406086ae8a04c6f7028581e2
ryan <ryan.playeralpha@gmail.com>
2023-03-16 13:33:00 +0800

    Update Plane.cpp

--- core/math/Plane.cpp
+++ core/math/Plane.cpp
@@ -8,6 +8,15 @@ const Plane Plane::cZXPlane( 0.0f, 1.0f, 0.0f, 0.0f );
 
 _bool Plane::Intersect( const Vector3& v1, const Vector3& v2, Vector3& p ) const
 {
-	// TODO.
-	return _false;
+	_float p1 = Distance( v1 ), p2 = Distance( v2 );
+	if ( p1 * p2 >= 0.0f )
+		return _false;
+
+	Ray ray( v1, ( v2 - v1 ).Normalize( ) );
+	_float dist;
+	if ( ray.Intersect( *this, dist ) == _false )
+		return _false;
+
+	p = ray.VectorOnRay( dist );
+	return _true;
 }
