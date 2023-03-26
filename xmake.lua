a5917da40f84b0ad1231263ddb8544e7a3e4dc93
ryan <ryan.playeralpha@gmail.com>
2023-03-26 18:13:42 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,9 @@
 
-target("core_math")
+target("core")
     set_kind("static")
 
+    add_files("core/base/*.cpp")
     add_files("core/math/*.cpp")
     add_includedirs("core/base")
+    add_includedirs("core/math")
     add_includedirs("core")
