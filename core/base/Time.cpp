3165eb17d70b29c5325fda9f963509bc1997c399
ryan <ryan.playeralpha@gmail.com>
2023-03-27 00:15:44 +0800

    Update Time.cpp

--- core/base/Time.cpp
+++ core/base/Time.cpp
@@ -41,8 +41,9 @@ _dword Time::GetCurrentTickcount( )
 
 _dword Time::GetElapsedSecondSinceUTC( )
 {
-	// TODO.
-	return 0;
+	time_t tt = 0;
+	::time( &tt );
+	return (_dword) tt;
 }
 
 Time::Time( StringPtr str )
@@ -92,7 +93,20 @@ Time::Time( StringPtr str )
 Time::Time( _large ms, _bool utc )
 	: year( 0 ), month( 0 ), dayofweek( 0 ), day( 0 ), hour( 0 ), minute( 0 ), second( 0 ), millisecond( 0 )
 {
-	// TODO.
+	_large ss = ms / 1000;
+
+	tm* tt = utc ? gmtime( (const time_t *) &ss ) : localtime( (const time_t *) &ss );
+	if ( tt != _null )
+	{
+		year			= 1900 + (_short) tt->tm_year;
+		month			= (_short) tt->tm_mon + 1;
+		dayofweek		= (_short) tt->tm_wday;
+		day				= (_short) tt->tm_mday;
+		hour			= (_short) tt->tm_hour;
+		minute			= (_short) tt->tm_min;
+		second			= (_short) tt->tm_sec;
+		millisecond		= ms % 1000;
+	}
 }
 
 _bool Time::operator > ( const Time& time ) const
@@ -244,6 +258,17 @@ String Time::ToString( ) const
 
 _large Time::ToLarge( _bool utc ) const
 {
-	// TODO.
-	return 0;
+	tm tt;
+	tt.tm_year	= year - 1900;
+	tt.tm_mon	= month - 1;
+	tt.tm_wday	= dayofweek;
+	tt.tm_mday	= day;
+	tt.tm_hour	= hour;
+	tt.tm_min	= minute;
+	tt.tm_sec	= second;
+
+	if ( utc )
+		return (_large) ::_mkgmtime( &tt ) * 1000 + millisecond;
+	else
+		return (_large) ::mktime( &tt ) * 1000 + millisecond;
 }
