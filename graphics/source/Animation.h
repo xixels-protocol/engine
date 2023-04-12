55d530c50f97c00e17bbf9baa9837c821fdf5f76
ryan <ryan.playeralpha@gmail.com>
2023-04-12 12:24:58 +0800

    Update Animation.h

--- graphics/source/Animation.h
+++ graphics/source/Animation.h
@@ -21,26 +21,26 @@ protected:
 
 	_bool	ReadyForUpdate( _dword& elapse );
 	_void	Update( _dword elapse );
-};
 
-};
-
-#define _IMPLEMENT_ANIMAION \
-	virtual _void SetDuration( _dword duration ) \
-		{ mDuration = duration; } \
-	virtual _dword GetDuration( ) const \
-		{ return mDuration; } \
-	virtual _dword GetCurrent( ) const \
-		{ return mCurrent; } \
-	virtual _void SetPlayMode( _dword mode ) \
-		{ mPlayMode = mode; } \
-	virtual _dword GetPlayMode( ) const \
-		{ return mPlayMode; } \
-	virtual _void SetSpeed( _float speed ) \
-		{ mSpeed = speed; } \
-	virtual _float GetSpeed( ) const \
-		{ return mSpeed; } \
-	virtual _bool ReachEnd( ) const \
-		{ return mReachEnd; } \
-	virtual _void Reset( ) \
+public:
+	virtual _void SetDuration( _dword duration )
+		{ mDuration = duration; }
+	virtual _dword GetDuration( ) const
+		{ return mDuration; } 
+	virtual _dword GetCurrent( ) const
+		{ return mCurrent; }
+	virtual _void SetPlayMode( _dword mode )
+		{ mPlayMode = mode; }
+	virtual _dword GetPlayMode( ) const
+		{ return mPlayMode; }
+	virtual _void SetSpeed( _float speed )
+		{ mSpeed = speed; }
+	virtual _float GetSpeed( ) const
+		{ return mSpeed; }
+	virtual _bool ReachEnd( ) const
+		{ return mReachEnd; }
+	virtual _void Reset( )
 		{ mCurrent = 0; mReachEnd = _false; mAdjustElapse = 0.0f; }
+};
+
+};
