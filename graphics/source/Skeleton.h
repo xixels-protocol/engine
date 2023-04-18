8f554a25396c4d6c10599b288dc2a75019614fe7
ryan <ryan.playeralpha@gmail.com>
2023-04-18 22:29:15 +0800

    Update Skeleton.h

--- graphics/source/Skeleton.h
+++ graphics/source/Skeleton.h
@@ -18,6 +18,19 @@ public:
 		{ if ( mResource != _null ) return; mResource = res; if ( res != _null ) res->IncRefCount( ); }
 
 	_void UpdateBones( _dword elapse = 0 );
+
+	virtual _dword GetBoneCount( ) const
+		{ return mBones.Length( ); }
+	virtual IBone* GetBone( _dword index ) const
+		{ return mBones.IsIndexValid( (_long) index ) ? mBones[ index ] : _null; }
+
+	virtual IBone*			CreateBone( StringPtr name, _dword parentid, _bool dummy );
+	virtual _void			ReleaseBone( IBone*& bone );
+	virtual _void			ClearBone( );
+
+	virtual IBone*			GetBone( StringPtr name ) const;
+
+	virtual _void			Skinning( Geometry& geo );
 };
 
 };
