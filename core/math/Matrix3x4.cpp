0e55a07ff14a9db87cd358adb695bae85f44714b
ryan <ryan.playeralpha@gmail.com>
2023-04-21 23:18:44 +0800

    Create Matrix3x4.cpp

---
+++ core/math/Matrix3x4.cpp
@@ -0,0 +1,4 @@
+#include "Matrix3x4.h"
+
+const Matrix3x4 Matrix3x4::cZero( 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f );
+const Matrix3x4 Matrix3x4::cIdentity( 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f );
