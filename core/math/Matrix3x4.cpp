0eef597d371dd2c9da316b4000f8801b023776dc
ryan <ryan.playeralpha@gmail.com>
2023-04-21 23:19:16 +0800

    Update Matrix3x4.cpp

--- core/math/Matrix3x4.cpp
+++ core/math/Matrix3x4.cpp
@@ -2,3 +2,28 @@
 
 const Matrix3x4 Matrix3x4::cZero( 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f );
 const Matrix3x4 Matrix3x4::cIdentity( 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f );
+
+Matrix3x4& Matrix3x4::operator += ( const Matrix3x4& mat )
+{
+	m[0][0] += mat.m[0][0]; m[0][1] += mat.m[0][1]; m[0][2] += mat.m[0][2]; m[0][3] += mat.m[0][3];
+	m[1][0] += mat.m[1][0]; m[1][1] += mat.m[1][1]; m[1][2] += mat.m[1][2]; m[1][3] += mat.m[1][3];
+	m[2][0] += mat.m[2][0]; m[2][1] += mat.m[2][1]; m[2][2] += mat.m[2][2]; m[2][3] += mat.m[2][3];
+
+	return *this;
+}
+
+Matrix3x4& Matrix3x4::operator *= ( _float s )
+{
+	m[0][0] *= s; m[0][1] *= s; m[0][2] *= s; m[0][3] *= s;
+	m[1][0] *= s; m[1][1] *= s; m[1][2] *= s; m[1][3] *= s;
+	m[2][0] *= s; m[2][1] *= s; m[2][2] *= s; m[2][3] *= s;
+
+	return *this;
+}
+
+_bool Matrix3x4::operator == ( const Matrix3x4& mat ) const
+{
+	return	m[0][0] == mat.m[0][0] && m[0][1] == mat.m[0][1] && m[0][2] == mat.m[0][2] && m[0][3] == mat.m[0][3] &&
+			m[1][0] == mat.m[1][0] && m[1][1] == mat.m[1][1] && m[1][2] == mat.m[1][2] && m[1][3] == mat.m[1][3] &&
+			m[2][0] == mat.m[2][0] && m[2][1] == mat.m[2][1] && m[2][2] == mat.m[2][2] && m[2][3] == mat.m[2][3];
+}
