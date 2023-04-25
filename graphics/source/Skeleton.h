e4a45bd87e39a97826e1290c70abebe17d72fa66
ryan <ryan.playeralpha@gmail.com>
2023-04-25 17:44:19 +0800

    Update Skeleton.h

--- graphics/source/Skeleton.h
+++ graphics/source/Skeleton.h
@@ -6,6 +6,18 @@ namespace Xixels
 class Skeleton : public ISkeleton, public ReferencedObject
 {
 private:
+	struct AffectAnima
+	{
+		ISkeletonAnima*	mAnima;
+		_float			mTime;
+		_float			mWeight;
+
+		inline AffectAnima( )
+			{ }
+		inline AffectAnima( ISkeletonAnima* anima, _float time, _float weight )
+			: mAnima( anima ), mTime( time ), mWeight( weight ) { }
+	};
+
 	Skeleton*				mResource;
 	AxisAlignedBox			mBoundBox;
 
@@ -18,6 +30,7 @@ private:
 
 	_dword					mFrameToken;
 	_bool					mNeedToUpdate;
+	Array< AffectAnima >	mAffectAnimas;
 public:
 	Skeleton( );
 	Skeleton( Skeleton* skeleton );
@@ -28,6 +41,15 @@ public:
 
 	inline const Matrix3x4* GetSkinTransform( ) const
 		{ return mSkinTransform; }
+	inline _void NeedToUpdate( )
+		{ mNeedToUpdate = _true; }
+
+	inline _void AllocateBuffer( _dword bonecount )
+		{ mMemoryBuffer.CreateBuffer( bonecount * sizeof( Bone ) ); }
+
+	_void AnimateBones( ISkeletonAnima* anima, _float time, _float weight );
+	_void UpdateSkinTransform( );
+	_void UpdateBoundBox( );
 	_void UpdateBones( _dword elapse = 0 );
 
 public:
@@ -40,17 +62,21 @@ public:
 		{ mFrameToken = token; }
 	virtual _dword GetFrameToken( ) const
 		{ return mFrameToken; }
+
+	virtual _dword GetSkinBoneCount( ) const
+		{ return mSkinBoneCount; }
 	virtual const AxisAlignedBox& GetBoundBox( ) const
 		{ return mBoundBox; }
+		
 	virtual IBone*			CreateBone( StringPtr name, _dword parentid, _bool dummy );
 	virtual _void			ReleaseBone( IBone*& bone );
 	virtual _void			ClearBone( );
 
 	virtual IBone*			GetBone( StringPtr name ) const;
 
-	virtual _dword			GetSkinBoneCount( ) const;
 	virtual _void			TransferSkinTransform( Geometry& geo ) const;
 
+	virtual _void			UsingSkeleton( _dword priority );
 	virtual _void			Skinning( Geometry& geo );
 
 };
