c0d3f4c93c7088e83d6691016864a960c0b37472
ryan <ryan.playeralpha@gmail.com>
2023-05-09 17:07:35 +0800

    Update VertexBuffer.cpp

--- graphics/source/VertexBuffer.cpp
+++ graphics/source/VertexBuffer.cpp
@@ -28,3 +28,78 @@ VertexBuffer::VertexBuffer( VertexBuffer* buffer ) : mFrameToken( 0 ), mUserData
 VertexBuffer::~VertexBuffer( )
 {
 }
+
+_void VertexBuffer::ReleaseResourceData( )
+{
+	if ( mResourceData->DecRefCount( ) == 0 )
+	{
+		if ( mResourceData->mResObject != _null )
+		{
+			GetRenderer( ).ReleaseVertexBuffer( mResourceData->mResObject );
+			( (GeometryFactory&) GetGeometryFactory( ) ).DecreaseVertexBufferSize( IGeometryFactory::_BUFFER_RENDER, mResourceData->mLength );
+		}
+
+		ReleaseMemoryBuffer( );
+
+		delete mResourceData;
+	}
+}
+
+_void VertexBuffer::ResetResourceData( VertexBuffer* buffer )
+{
+	if ( mResourceData == buffer->mResourceData )
+		return;
+
+	ReleaseResourceData( );
+
+	mResourceData = buffer->mResourceData;
+	mResourceData->IncRefCount( );
+}
+
+_void VertexBuffer::CreateMemoryBuffer( )
+{
+	_byte* rbuffer = _null;
+	_byte* abuffer = _null;
+
+	// Align is 1, so just create buffer in any address.
+	if ( mResourceData->mAlign == 1 )
+	{
+		rbuffer = new _byte[ mResourceData->mLength ];
+		abuffer = rbuffer;
+		( (GeometryFactory&) GetGeometryFactory( ) ).IncreaseVertexBufferSize( IGeometryFactory::_BUFFER_SYSTEM, mResourceData->mLength );
+	}
+	// Align is not 1, create buffer a litte larger, and adjust align buffer.
+	else
+	{
+		rbuffer = new _byte[ mResourceData->mLength + mResourceData->mAlign ];
+		abuffer = (_byte*) ( ( (_ptr) rbuffer + mResourceData->mAlign - 1 ) / mResourceData->mAlign * mResourceData->mAlign );
+		( (GeometryFactory&) GetGeometryFactory( ) ).IncreaseVertexBufferSize( IGeometryFactory::_BUFFER_SYSTEM, mResourceData->mLength + mResourceData->mAlign );
+	}
+
+	mResourceData->mRawBuffer = rbuffer;
+	mResourceData->mAlignBuffer = abuffer;
+}
+
+_void VertexBuffer::ReleaseMemoryBuffer( )
+{
+	if ( mResourceData->mRawBuffer == _null )
+		return;
+
+	_dword size = GetMemoryBufferSize( );
+
+	delete[] mResourceData->mRawBuffer;
+	mResourceData->mRawBuffer = _null;
+	mResourceData->mAlignBuffer = _null;
+
+	( (GeometryFactory&) GetGeometryFactory( ) ).DecreaseVertexBufferSize( IGeometryFactory::_BUFFER_SYSTEM, size );
+}
+
+_dword VertexBuffer::GetMemoryBufferSize( ) const
+{
+	if ( mResourceData->mRawBuffer == _null )
+		return 0;
+	else if ( mResourceData->mAlign == 1 )
+		return mResourceData->mLength;
+	else
+		return mResourceData->mLength + mResourceData->mAlign;
+}
