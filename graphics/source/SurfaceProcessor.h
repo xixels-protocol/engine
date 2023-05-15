9de7dcd9e4f52c70ac9f9c6a5cfc17bc92559ecf
ryan <ryan.playeralpha@gmail.com>
2023-05-15 09:49:51 +0800

    Update SurfaceProcessor.h

--- graphics/source/SurfaceProcessor.h
+++ graphics/source/SurfaceProcessor.h
@@ -27,4 +27,13 @@ public:
 	_void CopyFrom( _dword x, _dword y, const _byte* buf, _dword length );
 };
 
+class SurfaceProcessor
+{
+public:
+	static _void	ProcessHSL( _byte* buffer, _dword pitch, _dword hue, _dword saturation, _long lightness, const Rect& rect );
+	static _void	ProcessRGB( _byte* buffer, _dword pitch, _dword modulate, _dword additive, _dword subtract, const Rect& rect );
+	static _void	ProcessBorder( _byte* buffer, _dword pitch, _dword color, _dword bordersize, _float blurrate, const Rect& rect );
+	static _void	ProcessBorder( _byte* buffer, _dword pitch, const Rect& rect );
+};
+
 };
